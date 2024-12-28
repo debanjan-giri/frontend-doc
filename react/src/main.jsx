647d82a2be993d71c9cdb7c998b5cc12d2722507
Debanjan <debanjan.py@gmail.com>
2024-12-28 18:09:50 +0530

    msg

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Basic } from './Basic';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Basic />
   </StrictMode>,
 )
