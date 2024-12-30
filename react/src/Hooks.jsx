7aebbb21776477c3c2187f2598441053fd5e90a3
Debanjan <debanjan.py@gmail.com>
2024-12-30 11:54:27 +0530

    m

--- react/src/Hooks.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Hooks = () => {
-  return (
-    <div>Hooks</div>
-  )
-}
-
-export default Hooks
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Basic } from './Basic';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Component_JSX from "./_react_fundamental/Component_JSX";
+import Hook_customHook from "./_react_fundamental/Hook_customHook";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Basic />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <>
+    <Hook_customHook />
+  </>
+);
